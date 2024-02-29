2a3cdf773d3b0345f7040b0659c47086b28022f3
ooherin <555ohr@naver.com>
2024-02-29 19:23:53 +0900

    refactor: 영화 데이터 동적으로 불러오도록 수정

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { movies } from "../data/movies.js";
+
 const nameInput = document.querySelector("#name-input");
 
 document.getElementById("submit-button").addEventListener("click", () => {
@@ -7,4 +9,44 @@ document.getElementById("submit-button").addEventListener("click", () => {
   );
 });
 
-document.getElementById("cancel-button");
+const movieDataHTMLTemplate = (id, title, posterUrl, detail, link) => {
+  return `
+  <tr>
+    <td>${id}</td>
+    <td>${title}</td>
+    <td>
+      <img
+        src=${posterUrl}
+        class="poster"
+        alt=${title} 포스터
+      />
+    </td>
+    <td>${detail}</td>
+    <td><a href=${link}>바로가기</a></td>
+  </tr>
+`;
+};
+
+const movieDataHTML = movies
+  .map((movie) => {
+    const { id, title, posterUrl, detail, link } = movie;
+    return movieDataHTMLTemplate(id, title, posterUrl, detail, link);
+  })
+  .join("");
+
+const tbody = document.querySelector("tbody");
+tbody.insertAdjacentHTML("beforeend", movieDataHTML);
+
+const movieSelectHTMLTemplate = (title, enTitle) => {
+  return `<span>${title}</span>
+ <input type="radio" name=${enTitle} />`;
+};
+
+const movieSelectHTML = movies
+  .map((movie) => {
+    return movieSelectHTMLTemplate(movie.title, movie.enTitle);
+  })
+  .join("");
+
+const selectMovieBox = document.querySelector("#select-like-movies-box");
+selectMovieBox.insertAdjacentHTML("beforeend", movieSelectHTML);
